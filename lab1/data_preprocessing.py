655fa00b4baee927af7351eaf969a4bbf7e3f172
Solodnikova Sophie <147924281+SolodnikovaSophie@users.noreply.github.com>
2025-03-01 15:00:49 +0500

    Update data_preprocessing.py

--- lab1/data_preprocessing.py
+++ lab1/data_preprocessing.py
@@ -3,15 +3,15 @@ import pickle
 from sklearn.preprocessing import StandardScaler
 
 def preprocessing_data():
-    X_train = np.load("lab1/train/X_train.npy")
-    X_test = np.load("lab1/test/X_test.npy")
+    X_train = np.load("lab1/X_train.npy")
+    X_test = np.load("lab1/X_test.npy")
 
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
 
-    np.save("lab1/train/X_train_scaled.npy", X_train_scaled)
-    np.save("lab1/test/X_test_scaled.npy", X_test_scaled)
+    np.save("lab1/X_train_scaled.npy", X_train_scaled)
+    np.save("lab1/X_test_scaled.npy", X_test_scaled)
 
     with open("lab1/scaler.pkl", "wb") as f:
         pickle.dump(scaler, f)
