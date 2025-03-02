a35de98418ddf66b125308763e4bc496dc2abee2
SeriousDim <dlykov2001@yandex.ru>
2025-03-02 22:02:19 +0500

    Тестирование модели, requirements.txt в lab1

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -1,15 +1,18 @@
 import numpy as np
+import pickle
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from data_creation import import_data
 
-X_test = np.load('test/X_scaled.npy')
-y_test = np.load('test/y.npy')
+X_test = np.load('./test/X_scaled.npy')
+y_test = np.load('./test/y.npy')
 
-results = y_test # results = model.predict(X_test)
+with open("model.pkl", "rb") as file:
+    model = pickle.load(file)
+results = model.predict(X_test)
 
-accuracy = accuracy_score(y_test, y_test)
-precision = precision_score(y_test, y_test)
-recall = recall_score(y_test, y_test)
+accuracy = accuracy_score(y_test, results)
+precision = precision_score(y_test, results)
+recall = recall_score(y_test, results)
 
 print("Model test accuracy is: {}".format(accuracy))
 print("Model test precision is: {}".format(precision))
