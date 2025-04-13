69ef17a0fc3a99bdb2a6ff00a2f9329605816b80
kiD-9 <d.kirill.ekb@gmail.com>
2025-04-13 23:36:51 +0500

    fillna for dvc

--- lab4/modify_dataset.py
+++ lab4/modify_dataset.py
@@ -5,3 +5,7 @@ modified = pd.read_csv("lab4/titanic_raw.csv")
 modified = modified[["Pclass", "Sex", "Age"]]
 
 modified.to_csv("lab4/titanic_raw.csv")
+
+mean_age = modified['Age'].mean()
+modified['Age'] = modified['Age'].fillna(mean_age)
+modified.to_csv("lab4/titanic_raw.csv")
