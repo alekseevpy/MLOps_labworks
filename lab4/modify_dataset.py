d417f62f6d0cbe028c866133e2a6dffe7a267e30
Алексеев Лев <futuristjp@gmail.com>
2025-04-14 00:49:04 +0500

    New sign with one-hot-encoding

--- lab4/modify_dataset.py
+++ lab4/modify_dataset.py
@@ -4,8 +4,10 @@ modified = pd.read_csv("lab4/titanic_raw.csv")
 
 modified = modified[["Pclass", "Sex", "Age"]]
 
-modified.to_csv("lab4/titanic_raw.csv")
+mean_age = modified["Age"].mean()
+modified["Age"] = modified["Age"].fillna(mean_age)
 
-mean_age = modified['Age'].mean()
-modified['Age'] = modified['Age'].fillna(mean_age)
+sex_encoded = pd.get_dummies(modified["Sex"], prefix="Sex")
+modified = pd.concat([modified.drop("Sex", axis=1), sex_encoded], axis=1)
+
 modified.to_csv("lab4/titanic_raw.csv")
